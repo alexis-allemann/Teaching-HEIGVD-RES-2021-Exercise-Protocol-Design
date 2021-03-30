aa7628b1b9d90ae7e5d54fc8f1909aca024f030d
alexis-allemann <alexis.allemann@gmail.com>
2021-03-30 19:45:44 +0200

    Still a bug

--- src/main/java/ch/heigvd/res/labo3/tcpserver/Server.java
+++ src/main/java/ch/heigvd/res/labo3/tcpserver/Server.java
@@ -6,12 +6,12 @@ public class Server {
     final static Logger LOG = Logger.getLogger(Server.class.getName());
     final static int PORT = 9999;
 
-    public void serveClients() {
+    public static void serveClients() {
         LOG.info("Starting the Receptionist Worker on a new thread...");
         new Thread(new CalculatorWorker()).start();
     }
 
     public static void main(String[] args) {
-        System.out.println("test");
+        serveClients();
     }
 }
